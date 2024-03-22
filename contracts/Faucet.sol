031c94bb6ebe8e0ec7e5d4b661c70e04def19853
Makinde Oluwasegun <bryanconquer@gmail.com>
2024-03-22 16:47:28 +0100

    faucet_dapp

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -1,3 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+contract DaxFaucet {
+
+    address public owner;
+
+    mapping (address => uint256) public usersWithdrawAdd;
+
+    event deposit (address indexed sender, uint256 amount);
+
+    constructor () {
+       owner = msg.sender;
+    }
+
+    function depositFaucet (uint256 _amount) public payable {
+        require(msg.value > 0, "Invalid Amount");
+
+        emit deposit(msg.sender, _amount);
+    }
+
+    function faucetBalance () public view returns (uint256) {
+        return balance(address(owner));
+    }
+
+    function withDrawFaucet () public payable returns (uint256) {
+        require(address.(this) > 0.01 ether, "You must have more then 0.01 ether in your wallet");
+        address(this).timeStamp()
+        return transfer(owner, 0.01).address.(this);
+    }
+
+}
