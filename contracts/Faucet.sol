fa90681c5b20b8299813bac27c8809fc4ce25875
Makinde Oluwasegun <bryanconquer@gmail.com>
2024-03-25 15:29:32 +0100

    faucet-app

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -23,8 +23,16 @@ contract DaxFaucet {
         return address(this).balance;
     }
 
+    function faucetWithDrawTime (address _address) public view returns (bool) { 
+        return usersWithdrawTime[_address] + 1 days <= block.timestamp;
+    }
+
     function withDrawFaucet () public payable {
         require(msg.value > 0.01 ether, "You must have more then 0.01 ether in your wallet");
+        require(faucetWithDrawTime(msg.sender), "You can only withdraw in 24 hours");
+
+        usersWithdrawTime[msg.sender] = block.timestamp;
+
         emit transfer(address(this), msg.sender, 0.01 ether);
     }
 
