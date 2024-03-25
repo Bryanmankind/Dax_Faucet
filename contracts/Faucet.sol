ca31c582b02d5e253a71d67b155e5995c9563dc9
Makinde Oluwasegun <bryanconquer@gmail.com>
2024-03-25 15:58:04 +0100

    faucet-app

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -5,35 +5,34 @@ contract DaxFaucet {
 
     address public owner;
 
-    mapping (address => uint256) public usersWithdrawAdd;
+    mapping (address => uint256) public usersWithdrawTime;
 
-    event deposit (address indexed sender, uint256 amount);
+    event deposit (address indexed sender, uint256 _amount);
 
     constructor () {
        owner = msg.sender;
     }
 
     function depositFaucet (uint256 _amount) public payable {
-        require(msg.value > 0, "Invalid Amount");
-
-        emit deposit(msg.sender, _amount);
+        require(msg.value > 0, "Invalid amount");
+        emit  deposit(msg.sender, _amount);
     }
 
     function faucetBalance () public view returns (uint256) {
         return address(this).balance;
     }
 
-    function faucetWithDrawTime (address _address) public view returns (bool) { 
-        return usersWithdrawTime[_address] + 1 days <= block.timestamp;
+    function faucetWithDrawTime (address _address) public view returns (bool) {
+        return usersWithdrawTime[_address] + 1 minutes < block.timestamp;
     }
 
     function withDrawFaucet () public payable {
-        require(msg.value > 0.01 ether, "You must have more then 0.01 ether in your wallet");
         require(faucetWithDrawTime(msg.sender), "You can only withdraw in 24 hours");
 
         usersWithdrawTime[msg.sender] = block.timestamp;
 
-        emit transfer(address(this), msg.sender, 0.01 ether);
+        (bool success, ) = msg.sender.call{value: 1 ether}("");
+        require(success, "Transfer failed.");
     }
 
 }
