4152198a723889a54728a9eec24caca049ccd063
Makinde Oluwasegun <bryanconquer@gmail.com>
2024-03-25 16:56:58 +0100

    faucet-app

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -1,16 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface  IERC20 {
+        function transfer (address to, uint256 amount) external view returns (bool);
+        function balanceOf (address _acount) external view returns (uint256);
+    }
+
 contract DaxFaucet {
 
-    address public owner;
+    address payable  owner;
+    IERC20 public  token;
+
+    uint256 public withDrawAmount = 50 * (10 ** 18);
 
     mapping (address => uint256) public usersWithdrawTime;
 
     event deposit (address indexed sender, uint256 _amount);
 
-    constructor () {
-       owner = msg.sender;
+    constructor (address _tokenAddress){
+        token = IERC20(_tokenAddress);
+       owner = payable (msg.sender);
     }
 
     function depositFaucet (uint256 _amount) public payable {
@@ -26,7 +38,10 @@ contract DaxFaucet {
         return usersWithdrawTime[_address] + 1 minutes < block.timestamp;
     }
 
-    function withDrawFaucet () public payable {
+    function withDrawFaucet () public {
+
+        token.transfer(msg.sender, withDrawAmount);
+
         require(faucetWithDrawTime(msg.sender), "You can only withdraw in 24 hours");
 
         usersWithdrawTime[msg.sender] = block.timestamp;
