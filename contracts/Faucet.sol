6c0db17a93495e495fcd2888cdf5563e61765e6d
Makinde Oluwasegun <bryanconquer@gmail.com>
2024-03-25 17:55:58 +0100

    faucet-app

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -7,45 +7,41 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 interface  IERC20 {
         function transfer (address to, uint256 amount) external view returns (bool);
         function balanceOf (address _acount) external view returns (uint256);
+        event Transfer(address indexed from, address indexed  to, uint256 value);
     }
 
 contract DaxFaucet {
 
     address payable  owner;
     IERC20 public  token;
+    uint256 public lockTime = 1 minutes;
 
     uint256 public withDrawAmount = 50 * (10 ** 18);
 
     mapping (address => uint256) public usersWithdrawTime;
 
-    event deposit (address indexed sender, uint256 _amount);
+    event deposit (address indexed sender, uint256 indexed _amount);
 
     constructor (address _tokenAddress){
         token = IERC20(_tokenAddress);
        owner = payable (msg.sender);
     }
 
-   receive() external payable {
+    receive() external payable {
         emit  deposit(msg.sender, msg.value);
     }
 
-
-    function faucetBalance () public view returns (uint256) {
-        return address(this).balance;
+    function faucetBalance () external  view returns (uint256) {
+        return  token.balanceOf(address(this));
     }
 
-    function faucetWithDrawTime (address _address) public view returns (bool) {
-        return usersWithdrawTime[_address] + 1 minutes < block.timestamp;
-    }
 
     function withDrawFaucet () public {
         require(msg.sender != address(0), "You need 0.001 ether in your wallet");
-
         require(token.balanceOf(address(this)) >= withDrawAmount, "Not enough faucet at the moment");
-
         require(block.timestamp >= usersWithdrawTime[msg.sender], "You can only withdraw in 24 hours");
-
         usersWithdrawTime[msg.sender] = block.timestamp + lockTime;
+
 
         token.transfer(msg.sender, withDrawAmount);
     }
@@ -55,7 +51,11 @@ contract DaxFaucet {
     }
 
     function reSetlockTime (uint256 _time) private onlyOwner {
-        lockTime = _time;
+        lockTime = _time * 1 minutes;
+    }
+
+    function withDraw () external view  onlyOwner {
+        token.transfer(msg.sender, token.balanceOf(address(this)));
     }
 
     modifier onlyOwner() {
