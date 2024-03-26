bc7a7aa409e30e5ba99c97c62ba520e1a0a26421
Makinde Oluwasegun <bryanconquer@gmail.com>
2024-03-26 15:34:43 +0100

    faucet-app

--- contracts/faucetToken.sol
+++ contracts/faucetToken.sol
@@ -9,7 +9,7 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract faucetToken is ERC20 {
     address payable owner;
 
-    constructor () ERC20(faucetToken, FCt){
+    constructor () ERC20("faucetToken", "FCT"){
         owner = payable(msg.sender);
         _mint(owner, 500000 * (10 ** decimals()))
 }
