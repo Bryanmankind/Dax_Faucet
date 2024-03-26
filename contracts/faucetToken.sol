749031af80b589e2039d1d86e6722582c6b31905
Makinde Oluwasegun <bryanconquer@gmail.com>
2024-03-26 15:42:50 +0100

    faucet-app

--- contracts/faucetToken.sol
+++ contracts/faucetToken.sol
@@ -3,13 +3,15 @@
 pragma solidity ^0.8.24;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Capped} from "@openzeppelin/contracts/token/ERC20/extension/ERC20Capped.sol"
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extension/ERC20Burnable.sol"
 
 
 
-contract faucetToken is ERC20 {
+contract faucetToken is ERC20Capped {
     address payable owner;
 
-    constructor () ERC20("faucetToken", "FCT"){
+    constructor (uint256 cap) ERC20("faucetToken", "FCT") ERC20Capped(cap * (10 ** decimals())) {
         owner = payable(msg.sender);
         _mint(owner, 500000 * (10 ** decimals()))
 }
